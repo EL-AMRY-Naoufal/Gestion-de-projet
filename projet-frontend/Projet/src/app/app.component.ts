5f316408e7c58e0827b580a2c51174a1d9e76f5c
Ghalia Lahlou <ghalia.lahlou4@etu.univ-lorraine.fr>
2024-10-29 14:04:12 +0100

    login works

--- projet-frontend/Projet/src/app/app.component.ts
+++ projet-frontend/Projet/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './componenets/login/login.component';
+import { FormsModule } from '@angular/forms';
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet ,LoginComponent ],
+  imports: [RouterOutlet ,LoginComponent  , FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- projet-frontend/Projet/src/app/app.module.ts
+++ projet-frontend/Projet/src/app/app.module.ts
@@ -1,21 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app.routes'
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './componenets/login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    
   ],
   imports: [
     BrowserModule,
     FormsModule,
-      HttpClientModule,
-    ReactiveFormsModule
+    HttpClientModule,
+  //  ReactiveFormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
