b6669b315268894f3650339a79b2a8445d5dc221
sabbagh <mohammad-wael.sabbagh6@etu.univ-lorraine.fr>
2024-10-31 17:14:02 +0100

    Create a modal to update a teacher

--- projet-frontend/Projet/src/app/componenets/dashboard/dashboard.component.ts
+++ projet-frontend/Projet/src/app/componenets/dashboard/dashboard.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { UpdateProfesseurComponent } from '../update-professeur/update-professeur.component';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [DashboardComponent],
+  imports: [DashboardComponent, RouterOutlet, MatIconModule, MatDialogModule, UpdateProfesseurComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
 
+  constructor(private dialog: MatDialog) {}
+
+  openDialog(enseignant?: any) {
+    const dialogRef = this.dialog.open(UpdateProfesseurComponent, {
+      data: enseignant,
+      width: '500px',  
+      autoFocus: true
+    });
+  
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        console.log('Données reçues du modal:', result);
+      }
+    });
+  }
 }
