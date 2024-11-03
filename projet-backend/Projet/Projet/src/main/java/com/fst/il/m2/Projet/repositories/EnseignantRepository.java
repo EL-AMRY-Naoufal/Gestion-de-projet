c23d1455fb57164f81f48e5b7e8398b50175ee9c
sabbagh <mohammad-wael.sabbagh6@etu.univ-lorraine.fr>
2024-11-03 19:25:05 +0100

    backend : get enseignant by id

--- projet-backend/Projet/Projet/src/main/java/com/fst/il/m2/Projet/repositories/EnseignantRepository.java
+++ projet-backend/Projet/Projet/src/main/java/com/fst/il/m2/Projet/repositories/EnseignantRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EnseignantRepository extends JpaRepository<Enseignant, User> {
+public interface EnseignantRepository extends JpaRepository<Enseignant, Long> {
 }
