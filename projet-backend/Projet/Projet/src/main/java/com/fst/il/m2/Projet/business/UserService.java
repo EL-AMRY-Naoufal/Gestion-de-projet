ce64b0aba8a96da93baabd8ca4bb771308a48247
RomainPol <romain.pol57@gmail.com>
2024-11-04 19:48:03 +0100

    added function to get logged user role

--- projet-backend/Projet/Projet/src/main/java/com/fst/il/m2/Projet/business/UserService.java
+++ projet-backend/Projet/Projet/src/main/java/com/fst/il/m2/Projet/business/UserService.java
@@ -1,6 +1,9 @@
 package com.fst.il.m2.Projet.business;
 
+import com.fst.il.m2.Projet.enumurators.Role;
+
 public interface UserService {
     public int authenticate(String email, String password);
     public void modifyPassword(Long id, String password);
+    public Role getUserRole(Long id);
 }
