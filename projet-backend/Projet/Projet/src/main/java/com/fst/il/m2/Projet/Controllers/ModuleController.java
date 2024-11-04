1955f7b0ddf65f04bb8381c5647241f51a3d54ab
RomainPol <romain.pol57@gmail.com>
2024-11-04 19:48:42 +0100

    added module assigned hours modification in backend

--- projet-backend/Projet/Projet/src/main/java/com/fst/il/m2/Projet/Controllers/ModuleController.java
+++ projet-backend/Projet/Projet/src/main/java/com/fst/il/m2/Projet/Controllers/ModuleController.java
@@ -4,12 +4,8 @@ import com.fst.il.m2.Projet.business.ModuleService;
 import com.fst.il.m2.Projet.business.UserService;
 import com.fst.il.m2.Projet.dto.ModuleDto;
 import com.fst.il.m2.Projet.enumurators.Role;
-import com.fst.il.m2.Projet.enumurators.TypeHeure;
-import com.fst.il.m2.Projet.models.User;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -23,13 +19,5 @@ public class ModuleController {
     public ModuleController(ModuleService moduleService, UserService userService) {
         this.moduleService = moduleService;
         this.userService = userService;
-    }
-
-    @PutMapping("/{currentUserId}/assignedHours")
-    public void modifyAssignedHours(@PathVariable Long currentUserId, @Valid @RequestBody ModuleDto moduleDto) {
-        if(!this.userService.getUserRole(currentUserId).equals(Role.ADMINISTRATEUR)) {
-            throw new RuntimeException("You do not have the required permissions to make these changes");
-        }
-        this.moduleService.modifyAssignedHours(moduleDto.getId(), moduleDto.getHeuresParType());
     }
 }
