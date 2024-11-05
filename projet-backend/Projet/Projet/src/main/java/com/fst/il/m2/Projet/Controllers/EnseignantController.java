cf599a22ff6e113adbc81d428ca6f6e27fa10260
hassanec3u <hassanec3u@etu.univ-lorraine.fr>
2024-11-05 20:04:15 +0100

    debut back end pour consultation des affectation pour un enseignant

---
+++ projet-backend/Projet/Projet/src/main/java/com/fst/il/m2/Projet/Controllers/EnseignantController.java
@@ -0,0 +1,31 @@
+package com.fst.il.m2.Projet.Controllers;
+import com.fst.il.m2.Projet.models.Affectation;
+import com.fst.il.m2.Projet.business.EnseignantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/api/enseignants")
+public class EnseignantController {
+
+    private final EnseignantService enseignantService;
+
+    @Autowired
+    public EnseignantController(EnseignantService enseignantService) {
+        this.enseignantService = enseignantService;
+    }
+
+    @GetMapping("/{id}/affectations")
+    public ResponseEntity<List<Affectation>> getAffectationsByEnseignantId(@PathVariable Long id) {
+        List<Affectation> affectations = enseignantService.getAffectationsByEnseignantId(id);
+        if (affectations.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(affectations, HttpStatus.OK);
+    }
+}
---
+++ projet-backend/Projet/Projet/src/main/java/com/fst/il/m2/Projet/business/EnseignantService.java
@@ -0,0 +1,29 @@
+package com.fst.il.m2.Projet.business;
+
+import com.fst.il.m2.Projet.models.Affectation;
+import com.fst.il.m2.Projet.models.Enseignant;
+import com.fst.il.m2.Projet.repositories.AffectationRepository;
+import com.fst.il.m2.Projet.repositories.EnseignantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EnseignantService {
+
+    private final EnseignantRepository enseignantRepository;
+    private final AffectationRepository affectationRepository;
+
+    @Autowired
+    public EnseignantService(EnseignantRepository enseignantRepository, AffectationRepository affectationRepository) {
+        this.enseignantRepository = enseignantRepository;
+        this.affectationRepository = affectationRepository;
+    }
+
+    public List<Affectation> getAffectationsByEnseignantId(Long enseignantId) {
+        Enseignant enseignant = enseignantRepository.findById(enseignantId)
+                .orElseThrow(() -> new RuntimeException("Enseignant not found"));
+        return enseignant.getAffectations();
+    }
+}
