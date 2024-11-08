80d52eae09d3d60430a421564530d3889f6759e2
TheoFaedo <73793294+TheoFaedo@users.noreply.github.com>
2024-11-08 23:02:52 +0100

    fix login graphics

--- projet-frontend/Projet/src/app/app.routes.ts
+++ projet-frontend/Projet/src/app/app.routes.ts
@@ -10,6 +10,7 @@ export const routes: Routes = [{ path: '', component: LoginComponent },
   { path: 'enseignants', component: EnseignantsComponent },
   //route for my consulte-enseignant component
   { path: 'enseignants/affectations', component: AffectationListComponent },
+  { path: '**', component: LoginComponent }
 ];
 
 @NgModule({
