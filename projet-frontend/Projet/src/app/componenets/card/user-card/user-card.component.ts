d6eea3b5ab24d83c61ac2c1d1ab38c167ee980df
sabbagh <mohammad-wael.sabbagh6@etu.univ-lorraine.fr>
2024-11-22 22:47:21 +0100

    update and add teacher

--- projet-frontend/Projet/src/app/componenets/card/user-card/user-card.component.ts
+++ projet-frontend/Projet/src/app/componenets/card/user-card/user-card.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user-card.component.html',
-  styleUrl: './user-card.component.scss'
+  styleUrls: ['./user-card.component.scss']
 })
 export class UserCardComponent {
- @Input() user: any; 
+  @Input() user: any; 
+  @Input() openDialog!: (user?: any) => void;
+
+  onEditClick() {
+    if (this.openDialog) {
+      this.openDialog(this.user);
+    }
+  }
 }
