098cff00086dbfec778311c0da1aceac7393ed82
sabbagh <mohammad-wael.sabbagh6@etu.univ-lorraine.fr>
2024-11-22 22:46:21 +0100

    using add teacher button

--- projet-frontend/Projet/src/app/services/enseignant.service.ts
+++ projet-frontend/Projet/src/app/services/enseignant.service.ts
@@ -2,12 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import {AffectationType} from "../Type/Affectation";
+import { environment } from '../../environments/environment';
+import { EnseignantDto } from '../types/enseignant.type';
+import { User } from '../types/user.types';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EnseignantService {
-  private apiUrl = 'http://localhost:8080/api/enseignants';
+  private apiUrl = `${environment.apiUrl}/enseignants`;
 
   constructor(private http: HttpClient) { }
 
@@ -17,5 +20,23 @@ export class EnseignantService {
 
   getUserByName(name: string): Observable<any[]> {
     return this.http.get<any[]>(`${this.apiUrl}?name=${name}`);
+  }
+  
+  getEnseignantsNotInEnseignantTable(): Observable<User[]> {
+    return this.http.get<User[]>(`${this.apiUrl}/enseignants-non-enregistres`);
+  }
+
+  getEnseignants(): Observable<User[]> {
+    return this.http.get<User[]>(`${this.apiUrl}`);
+  }
+
+  createEnseignant(enseignant: EnseignantDto): Observable<EnseignantDto> {
+    return this.http.post<EnseignantDto>(`${this.apiUrl}`, enseignant);
+  }
+  updateEnseignant(enseignant: EnseignantDto): Observable<EnseignantDto> {
+    return this.http.put<EnseignantDto>(`${this.apiUrl}`, enseignant);
+  }
+  getEnseignant(id: number): Observable<EnseignantDto> {
+    return this.http.get<EnseignantDto>(`${this.apiUrl}/${id}`);
   }
 }
