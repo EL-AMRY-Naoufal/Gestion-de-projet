7d51290884c3a8b639bbb77d6dbb826ef78328ba
sabbagh <mohammad-wael.sabbagh6@etu.univ-lorraine.fr>
2024-11-22 22:24:52 +0100

    Using Inheritance

--- projet-backend/Projet/Projet/src/main/java/com/fst/il/m2/Projet/mapper/EnseignantMapper.java
+++ projet-backend/Projet/Projet/src/main/java/com/fst/il/m2/Projet/mapper/EnseignantMapper.java
@@ -6,7 +6,7 @@ import com.fst.il.m2.Projet.models.Enseignant;
 public class EnseignantMapper {
     public static EnseignantDto enseignantToEnseignantDto(Enseignant enseignant) {
         return EnseignantDto.builder()
-                .id(enseignant.getUser().getId())
+                .id(enseignant.getId())
                 .categorie(enseignant.getCategorie())
                 .maxHeuresService(enseignant.getMaxHeuresService())
                 .heuresAssignees(enseignant.getHeuresAssignees())
