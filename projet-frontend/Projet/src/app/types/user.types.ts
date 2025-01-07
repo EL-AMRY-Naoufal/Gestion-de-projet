085c4e8c3fab17f4054defa0b9c651ea12fe2adc
madisonnoyer54 <115881298+madisonnoyer54@users.noreply.github.com>
2025-01-07 08:59:57 +0100

    debut de debug front

--- projet-frontend/Projet/src/app/types/user.types.ts
+++ projet-frontend/Projet/src/app/types/user.types.ts
@@ -1,8 +1,16 @@
 export interface User {
     id: number;
     username: string;
-    password: string; 
+    password: string;
     email: string;
-    role: 'CHEF_DE_DEPARTEMENT' | 'RESPONSABLE_DE_FORMATION' | 'SECRETARIAT_PEDAGOGIQUE' | 'ENSEIGNANT';
+    role: UserRoleDto[];
 }
+
+
+export interface UserRoleDto {
+  yearId: number; // Correspond au champ `Long yearId` dans le Java
+  role: 'CHEF_DE_DEPARTEMENT' | 'RESPONSABLE_DE_FORMATION' | 'SECRETARIAT_PEDAGOGIQUE' | 'ENSEIGNANT';  // Utilisation du type `Role` défini précédemment
+}
+
+
 
