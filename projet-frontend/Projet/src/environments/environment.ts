2b724ff02f8b5a105a2155be808c817cbf77d7c7
RomainPol <romain.pol57@gmail.com>
2025-03-01 19:08:30 +0100

    removed allAffectations route

--- projet-frontend/Projet/src/environments/environment.ts
+++ projet-frontend/Projet/src/environments/environment.ts
@@ -9,7 +9,7 @@ export const environment = {
     host: 'localhost',
     port: '8080',
     endpoints: {
-      allAffectation: '/api/affectations',
+      affectations: '/api/affectations',
       allUsers: '/api/responsableDepartement',
       oneUser: '/api/responsableDepartement/:id',
       allEnseignants: '/api/enseignants',
