1a4a4b3ab7aa4ce9ba8f6c472f6169453a0ffbd1
madisonnoyer54 <115881298+madisonnoyer54@users.noreply.github.com>
2025-03-03 22:37:09 +0100

    bouton OK

--- projet-frontend/Projet/src/app/components/affectation/affectation-dialog/affectation-dialog.component.ts
+++ projet-frontend/Projet/src/app/components/affectation/affectation-dialog/affectation-dialog.component.ts
@@ -1,14 +1,13 @@
 import { Component, Inject, Input, ViewEncapsulation } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { AffectationListComponent } from "../affectation-enseignant/affectation-enseignant.component";
-import { MatButtonModule } from "@angular/material/button";
-import { MatDialogActions, MatDialogTitle } from "@angular/material/dialog";
+import { MatDialogActions } from "@angular/material/dialog";
 import { User } from '../../shared/types/user.type';
 
 @Component({
   selector: 'app-affectation-dialog',
   standalone: true,
-  imports: [AffectationListComponent, MatDialogActions, MatDialogTitle, MatButtonModule],
+  imports: [AffectationListComponent,MatDialogActions],
   templateUrl: './affectation-dialog.component.html',
   styleUrls: ['./affectation-dialog.component.scss'],
   encapsulation: ViewEncapsulation.None,
